7560a3af3f6401e6da152a11f9596edb8cce10c4
Ali Gamal <alialashwall14@gmail.com>
2023-04-16 18:06:28 +0200

    Finish MainActivity

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-
 import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
